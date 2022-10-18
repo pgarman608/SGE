2b2f3c3dbddd4074b32c918c93be28b9435fa258
Pablo <instypablo@gmail.com>
2022-10-18 13:01:44 +0200

    Forma1 Terminada

--- Python/Formas/Python1.py
+++ Python/Formas/Python1.py
@@ -1,47 +1,126 @@
+from traceback import format_exc
+
+
 class Punto:
-    x
-    y
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-    def __int__(self,x,y):
+    x = 0
+    y = 0
+    def __init__(self,x=0,y=0):
         self.x = x
         self.y = y
+        pass
+
     def toString(self):
-        return "X: " + self.x + "e Y:" + self.y
+        return "X: " + str(self.x) + " e Y: " + str(self.y)
+
 class Forma:
-    __color
-    coord
-    nombre
-    def __init__(self):
-        self.color = ""
-        self.coord = Punto()
-        self.nombre = ""
-    def __init__(self, color, x,y,nombre):
+    __color = ""
+    coord = Punto()
+    nombre = ""
+    def __init__(self, color = "", x= 0,y = 0,nombre = ""):
         self.coord = Punto(x,y)
-        self.color = color
+        self.__color = color
         self.nombre = nombre
+
     def toString(self):
-        return "Nombre: " + self.nombre + "\nColor: " + self.color + "\nCoordenadas: " + self.coord.toString()
-    def setColor(self,color):
-        self.color = color
+        return "Nombre: " + self.nombre + "\nCoordenadas: " + self.coord.toString() + "\nColor: "+ self.__color
+
+    def setColor(self,color=""):
+        self.__color = color
+
     def getColor(self):
-        return self.color
-    def setCoord(self,x,y):
+        return self.__color
+
+    def setCoord(self,x=0,y=0):
         self.coord = Punto(x,y)
+
 class Rectangulo(Forma):
-    lMenor
-    lMayor
-    def __init__(self):
-        super()
-        self.lMenor = 0
-        self.lMayor = 0
-    def __int__(self,color,x,y,nombre,lMenor,LMayor):
-        super(Rectangulo, self).__int__(color,x,y,nombre)
+    lMenor = 0
+    lMayor = 0
+    def __init__(self, color="", x=0, y=0, nombre="",lMenor = 2,lMayor = 1):
+        super().__init__(color, x, y, nombre)
+        self.lMayor = lMayor
         self.lMenor = lMenor
-        self.lMayor = LMayor
+        pass
+
     def toString(self):
-        return "Nombre: " + self.nombre + "\nColor: " + self.color + "\nCoordenadas: " + self.coord.toString() + "\nLado Menor: " + self.lMenor + "\nLado Mayor: " + self.lMayor
-if __name__ == '__main__':
-    rectangulo = Rectangulo("Negro",10,10,"Campo","10","15")
-    print(rectangulo.toString())
+        return super().toString() + "\nLado menor: " + str(self.lMenor) + "\nLado mayor: " +  str(self.lMayor)
+
+    def area(self):
+        resul = self.lMayor * self.lMenor
+        return resul
+
+    def perimetro(self):
+        resul = self.lMayor * 2 + self.lMenor * 2
+        return resul
+
+    def cambiarTamanio(self,numero = 1):
+        self.lMenor = self.lMenor * numero
+        self.lMayor = self.lMayor * numero
+"""
+forma = Forma("Blanco",20,10,"Edificio1")
+print(forma.toString())
+forma.setColor("Azul")
+print(forma.getColor())
+forma.setCoord(10,20)
+print(forma.toString())
+rectangulo = Rectangulo("Negro",10,10,"marco",10,10)
+print(rectangulo.toString())
+rectangulo.setColor("Amarillo")
+print(rectangulo.getColor())
+rectangulo.setCoord(30,40)
+rectangulo.cambiarTamanio(4)
+print(rectangulo.toString())
+print(rectangulo.area())
+print(rectangulo.perimetro())
+"""
+class Elipse(Forma):
+    rMayor = 0
+    rMenor = 0
+    PI = 3.1416
+    def __init__(self, color="", x=0, y=0, nombre="",rMayor = 2,rMenor=1):
+        super().__init__(color, x, y, nombre)
+        self.rMayor = rMayor
+        self.rMenor = rMenor
+
+    def toString(self):
+        return super().toString() + "\nRadio Mayor: " + str(self.rMayor) +"\nRadio Menor: " + str(self.rMenor)
+
+    def area(self):
+        return self.PI * (self.rMayor * self.rMenor)
+
+class Cuadrado(Rectangulo):
+
+    def __init__(self, color="", x=0, y=0, nombre="", lado = 1):
+        super().__init__(color, x, y, nombre, lado, lado)
+
+    def toString(self):
+        return "Nombre: " + self.nombre + "\nCoordenadas: " + self.coord.toString() + "\nColor: "+ self.getColor() + "\nLado: " + str(self.lMayor)
+
+class Circulo(Elipse):
+
+    def __init__(self, color="", x=0, y=0, nombre="", radio = 1):
+        super().__init__(color, x, y, nombre, radio, radio)
+
+    def toString(self):
+        return "Nombre: " + self.nombre + "\nCoordenadas: " + self.coord.toString() + "\nColor: "+ self.getColor() + "\nRadio: " + str(self.rMayor)
+
+listaFiguras = list()
+forma = Forma("negro",10,10,"Edificio")
+rectangulo = Rectangulo("negro",10,10,"Campo",30,20)
+cuadrado = Cuadrado("negro",10,10,"Hectarea",10)
+elipse = Elipse("negro",10,10,"Tierra",10,9)
+circulo = Circulo("negro",10,10,"Pelota",7)
+listaFiguras.append(forma)
+listaFiguras.append(rectangulo)
+listaFiguras.append(cuadrado)
+listaFiguras.append(elipse)
+listaFiguras.append(circulo)
+
+for figura in listaFiguras:
+    
+    print(figura.toString())
+    figura.setColor("rojo")
+    figura.setCoord(20,20)
+    print(".-.-.")
+    print(figura.toString())
+    print("-----------------")
