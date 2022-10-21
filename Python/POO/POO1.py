0e954204ec575ae36ed4265f0fbc71da0e07a991
Pablo <instypablo@gmail.com>
2022-10-21 11:46:12 +0200

    Terminado v3

--- Python/POO/POO1.py
+++ Python/POO/POO1.py
@@ -10,15 +10,17 @@ class Persona():
         self.__sexo = sexo
 
     def calcularMC(self):
-        peso =  (self.__altura*self.__altura) / self.__peso
-        stn=""
-        if peso >= 1:
-            stn = "Tiene sobrepeso"
-        elif peso >= 0:
-            stn = "Tiene un peso ideal"
-        elif peso <= -1:
-            stn = "Tiene un peso bajo"
-        return stn
+        stn = 0
+        peso = 0
+        if self.__peso != 0 and self.__altura !=0:
+            peso =  self.__peso  / (float(self.__altura)*float(self.__altura))
+            if peso <= 17:
+                stn = 1
+            elif peso >=25.01:
+                stn = -1
+        else:
+            stn = -2
+        return stn,peso
 
     def esMayorDeEdad(self):
         aux = "Es mayor de edad"
@@ -43,26 +45,46 @@ class Persona():
             num += str(rnd.randint(1,9))
         dni = num + charDni[(int(num)%23)-1]
         return dni
+
     def setNombre(self,nombre):
         self.__nombre = nombre
+
     def setAltura(self,altura):
         self.__altura = altura
+
     def setEdad(self,edad):
         self.__edad = edad
+
     def setPeso(self,peso):
         self.__peso = peso
+
     def setSexo(self,sexo):
         self.__sexo = sexo
+
 """Programa"""
+def representarPeso(num):
+    aux,imc = num
+    stn = ""
+    if aux == -2:
+        stn = "No se puede calcular el peso ideal"
+    elif aux == -1:
+        stn = "Tiene muy poco peso"
+    elif aux == 0:
+        stn = "Esta en su peso ideal"
+    elif aux == 1:
+        stn = "Tiene sobrepeso"
+    return stn
 nombre = input("Introduce el nombre: ")
 edad = int(input("Introduce la edad: "))
 sexo = input("Introduce el sexo: ")
 peso = int(input("Introduce el peso: "))
 altura = float(input("Introduce la altura: "))
 persona1 = Persona(nombre,edad,peso,altura,sexo)
-persona2 = Persona(nombre,edad,0,0,sexo)
+persona2 = Persona(nombre,edad,sexo)
 persona3 = Persona()
-print(persona1.calcularMC())
+print(representarPeso(persona1.calcularMC()))
+print(representarPeso(persona2.calcularMC()))
+print(representarPeso(persona3.calcularMC()))
 print(persona1.esMayorDeEdad())
 print(persona2.esMayorDeEdad())
 print(persona3.esMayorDeEdad())
